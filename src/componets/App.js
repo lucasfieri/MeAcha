55a2d89a8cc8da6084591fbb224a41bfe974e77d
lucasfieri <lucas.pedroso18@etec.sp.gov.br>
2018-09-08 16:00:00 -0300

    Change logo

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../images/logo.png';
+// eslint-disable-next-line
+import css from '../css/splash.css';
+
 
 class App extends Component {
   render() {
@@ -8,11 +10,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started,teste <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
