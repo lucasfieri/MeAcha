4e37a70d61839a6fa684235301c2ddba0a93bf80
Lucas Fieri <lucasfieri@gmail.com>
2018-10-10 22:35:10 -0300

    posicionamento logo

--- src/componets/Splash.js
+++ src/componets/Splash.js
@@ -6,8 +6,12 @@ import css from '../css/splash.css';
 class Splash extends Component {
     render() {
         return (
-            <div className="Splash">
-              <img src={logo} className="App-logo" alt="logo" />
+            <div className="container">
+	            <div className="row">
+	            	<div className="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+						<img src={logo} id="logo" alt="logo" />
+	            	</div>
+	            </div>
             </div>
       );
     }
