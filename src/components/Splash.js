f82a5321566a175fda5a043f9ba7e7407fb888dd
Lucas Fieri <lucasfieri@gmail.com>
2018-11-18 20:42:40 -0200

    arrow function to splash

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
 import logo from '../images/logo.png';
-import  '../css/splash.css';
+import '../css/splash.css';
 
 class Splash extends Component {
-    componentDidMount(){
-        setTimeout(function() {
-            window.location.pathname="/Shell";
-        }, 3000);
-    }
-    render() {
-        return (
-            <div className="splash-container">  
-				<img src={logo} className="logo" alt="logo" />
-                
-            </div>
-      );
-    }
+  componentDidMount() {
+    setTimeout(() => {
+      window.location.pathname = "/Shell";
+    }, 3000);
+  }
+  render() {
+    return (
+      <div className="splash-container">
+        <img src={logo} className="logo" alt="logo" />
+      </div>
+    );
+  }
 }
 
 export default Splash;
