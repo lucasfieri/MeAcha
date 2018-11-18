1bc4340bdae7c168f43acde3ef74ed07b049b8d4
Lucas Fieri <lucasfieri@gmail.com>
2018-11-18 20:39:57 -0200

    change NavBar to stateless

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,44 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import BtnReturn from '../images/icons/btn-return.png'
 import '../css/reset.css';
 import '../css/default.css';
 import '../css/nav.css';
 import { Link } from "@reach/router";
 
-class NavBar extends Component {
-
-	render() {
-		if (this.props.type === 1) {
-			return (
-				<div className="container">
-					<nav>
-						<h1 className="title">Selecione um supermercado</h1>
-					</nav>
-				</div>
-			);
-		}
-		else if (this.props.type === 2) {
-			return (
-				<div className="container">
-					<nav>
-						<Link to="/Shell"><img src={BtnReturn} className="arrow-back" alt="arrow back icon"></img></Link>
-						<h1 className="title">{this.props.name}</h1>
-					</nav>
-				</div>
-			);
-		}
-		else {
-			return (
-				<div className="container">
-					<nav>
-						<Link to="/Shell"><img src={BtnReturn} className="arrow-back"  alt="arrow back icon"></img></Link>
-						<h1 className="title">Supermercado 1</h1>
-
-					</nav>
-				</div>
-			);
-		}
+function NavBar({ type }) {
+	if (type === 1) {
+		return(
+					<div className="container">
+			<nav>
+				<h1 className="title">Selecione um supermercado</h1>
+			</nav>
+		</div>
+		)
+	}
+	else if (type === 2) {
+		return (
+			<div className="container">
+				<nav>
+					<Link to="/Shell"><img src={BtnReturn} className="arrow-back" alt="arrow back icon"></img></Link>
+					<h1 className="title">{this.props.name}</h1>
+				</nav>
+			</div>
+		);
+	}
+	else {
+		return (
+			<div className="container">
+				<nav>
+					<Link to="/Shell"><img src={BtnReturn} className="arrow-back" alt="arrow back icon"></img></Link>
+					<h1 className="title">Supermercado 1</h1>
+				</nav>
+			</div>
+		)
 	}
 }
-
 export default NavBar;
--- src/components/Shell.js
+++ src/components/Shell.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Router } from "@reach/router";
 import Supermarket from './Supermarket';
 import SupermarketDetail from './SupermarketDetail';
-import SearchBar from './SearchBar';
+
 
 
 
@@ -11,11 +11,11 @@ class Shell extends Component {
   render() {
     return (
       <div>
-{/*          <Router>
+          <Router>
           <Supermarket path="/" />
           <SupermarketDetail path="Splash/Detail/:supermarketSelected"/>
-        </Router>  */}
-        <SearchBar />
+        </Router>  
+
       </div>
     );
   }
