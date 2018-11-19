e869196973eee398c104c751fd83bbcbb3896589
Lucas Fieri <lucasfieri@gmail.com>
2018-11-18 22:56:41 -0200

    css animation in splash

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from '../images/logo.png';
-import '../css/splash.css';
+import React, { Component } from 'react'
+import Logo from '../images/logo.png'
+import Star from '../images/star.png'
+import '../css/splash.css'
 
 class Splash extends Component {
   componentDidMount() {
@@ -11,7 +12,13 @@ class Splash extends Component {
   render() {
     return (
       <div className="splash-container">
-        <img src={logo} className="logo" alt="logo" />
+        <img src={Logo} className="logo" alt="Logotipo do Me Acha" />
+        <div className="star-container">
+          <img src={Star} className="star" id="star-1" alt="Estrelas" />
+          <img src={Star} className="star" id="star-2" alt="Estrelas" />
+          <img src={Star} className="star" id="star-3" alt="Estrelas" />
+        </div>
+
       </div>
     );
   }
